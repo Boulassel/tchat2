9b3aa31a51d9565409ef03997e170a52c6ad0f5b
MaAlexandre <masse.alexandre@hotmail.fr>
2015-04-07 13:39:54 +0200

    php requete insert

--- tchat_message.php
+++ tchat_message.php
@@ -2,8 +2,12 @@
 
 include './includes/config.php';
 include './includes/function.php';
+var_dump($_POST);
 
 if (array_key_exists('id', $_REQUEST))
     die(json_encode(newPDO()->prepare('SELECT * FROM messages WHERE id > :id')->execute($_REQUEST)));
 
-newPDO()->prepare('INSERT INTO messages (user,message) VALUES (:user,:message)')->execute($_REQUEST);
+newPDO()->prepare('INSERT INTO messages (user,message) VALUES (:user,:message)')->execute(array(
+    'user' => $_REQUEST['user'],
+    'message' => $_REQUEST['message'],
+));
