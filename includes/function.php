9706d6f82afd7ae57f97f3d0fb2d4220cdec1d2c
MaAlexandre <masse.alexandre@hotmail.fr>
2015-04-07 14:02:08 +0200

    moùjpoj

--- includes/function.php
+++ includes/function.php
@@ -1,7 +1,7 @@
 <?php
 function newPDO() {
     try {
-        $oPDO = new PDO('mysql:host=localhost;dbname=' . BDD, USER_BDD, MDP_BDD);
+        $oPDO = new PDO('mysql:host=192.168.3.93;dbname=' . BDD, USER_BDD, MDP_BDD);
     } catch (PDOException $ex) {
         echo '<br/>';
         echo "Echec lors de la connexion à MySQL : (" . $ex->getCode() . ") ";
