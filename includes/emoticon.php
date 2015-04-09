456688a1051c0c256706f6f021b023e67d478bf1
neoxyos <super_noki@live.fr>
2015-04-09 16:44:03 +0200

    emoticons finis

--- includes/emoticon.php
+++ includes/emoticon.php
@@ -3,13 +3,23 @@
 function emoT($msg)//emoticons
 {    
     $tab_emo = [
-        "oO", ";)","-_-", 
+        ":D", "><","><'",":$",">>",
+        "o.o","O.O",":(","<3)","<D","^b^",
+        ":'",":'(","(k)","-_-z",">u<",":*",
+        "oo'","<$","T-T",";)",":x",
     ];
     $chemin = "image/";
           
      $tab_lien = [
-       "confused.jpg", "wink.ico", "bored.png", 
-    ];
+       "Simley-1.ico", "Simley-2.ico", "Simley-3.ico", 
+         "Simley-4.ico","Simley-5.ico","Simley-7.ico",
+         "Simley-8.ico","Simley-9.ico","Simley-10.ico",
+         "Simley-11.ico","Simley-12.ico","Simley-13.ico",
+         "Simley-14.ico","Simley-16.ico","Simley-17.ico",
+         "Simley-18.ico","Simley-19.ico","Simley-20.ico",
+         "Simley-21.ico","Simley-23.ico","Simley-24.ico",
+         "Simley-25.ico","Simley-26.ico",
+         ];
     
 
     $tab_message = explode(" ", $msg);
