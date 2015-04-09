e636992797bebabf9ce29a174944262d6d59dd83
amadd <dabitaoamadou@gmail.com>
2015-04-09 10:35:49 +0200

    n'ajoutez plus rien

--- tchat.js
+++ tchat.js
@@ -1,16 +1,16 @@
-//function scrollaff()
-//{   
-//    $(document).ready(function()
-//    {
-//        $('#affichage').animate({ scrollTop: 999999999}, 500);
-//    });
+function scrollaff()
+{   
+    $(document).ready(function()
+    {
+        $('#affichage').animate({ scrollTop: 999999999}, 500);
+    });
     // var topPos = $('#affichage').offsetTop;
     // document.$('#affichage').scrollTop = topPos;
 
 // $("#affichage").animate({ 
 //     scrollTop: $('#affichage').offset().top - ($(window).height()-$("#affichage").outerHeight() +10)
 // }, 500);
-//}
+}
 
 //scrollaff();
 
@@ -20,7 +20,7 @@
 
 
 //php
-//// Ajout d'un nouveau message
+// Ajout d'un nouveau message
 // if (array_key_exists('envoyer', $_REQUEST)) {
 //    if($_REQUEST['user'] != "" && $_REQUEST['message'] =! "")
 //    {
